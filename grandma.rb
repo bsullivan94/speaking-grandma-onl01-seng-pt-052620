ad8684048c672fc8ea6f7b3b1bb728ff5d18b91b
brody sullivan <brodysullivan0@gmail.com>
2020-05-28 17:04:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,14 +12,3 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase)
-  "I LOVE YOU TOO PUMPKIN!"
-  # (if) the phrase (phrase) is not equal (!=) to the all uppercase version of the phrase (phrase.upcase) then they're not shouting and we should respond with "HUH?! SPEAK UP, SONNY!"
-  if phrase.upcase == 'I LOVE YOU GRANDMA!'
-  "I LOVE YOU TOO PUMPKIN"
-  elsif phrase != phrase.upcase
-  "HUH?! SPEAK UP, SONNY!"
-
-else
-  "NO, NOT SINCE 1938!"
-end
