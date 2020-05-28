978946f2e8eec85b042f98586fafafa0c92e996f
brody sullivan <brodysullivan0@gmail.com>
2020-05-28 16:49:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,6 @@ if phrase != phrase.upcase
   "HUH?! SPEAK UP SONNY!"
   elsif phrase == phrase.upcase && phrase == 'I LOVE YOU GRANDMA!'
   "NO, NOT SINCE 1938!"
-else
+  "I LOVE YOU TOO PUMPKIN!"
+else 
 end
