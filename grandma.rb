969a2135038f1298e4439a325a11db558cb82672
brody sullivan <brodysullivan0@gmail.com>
2020-05-28 16:56:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   "I LOVE YOU TOO PUMPKIN!"
 if phrase != phrase.upcase
   "HUH?! SPEAK UP SONNY!"
-  elsif phrase == phrase.upcase && phrase == 'I LOVE YOU GRANDMA!'
+  elsif phrase == 'I LOVE YOU GRANDMA!'
   "I LOVE YOU TOO PUMPKIN!"
 else
   "NO, NOT SINCE 1938!"
